36fefbf97ddbb18f299ab74de0a8a6a6cf095583
jaestrada40 <javiera.estradag@gmail.com>
2022-08-16 22:15:30 -0600

    nuevo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function init()
+{
+    document.getElementById("exampleFormControlTextarea1").focus();
+}
+
+
+
+function myFunction() {
+    document.getElementById("imagen").style.visibility = "hidden";
+    document.getElementById("parrafo").style.visibility = "visible";
+  }
